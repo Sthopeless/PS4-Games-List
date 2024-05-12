96fe7277bc82e71c60af17a0a0d68ff16e77dcac
sthope <rjs.ricardosilva@gmail.com>
2024-05-12 04:03:44 +0200

    Initial commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -21,7 +21,7 @@ $(function () {
                 "data": results.data,
                 "deferRender": true,
                 "scrollY": 550,
-                "pageLength": 1000,
+                "pageLength": -1,
                 "select": true,
                 "columnDefs": [{
                     "width": "0%",
